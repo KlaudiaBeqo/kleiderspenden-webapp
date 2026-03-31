244f26ad7c15cb35195f9d9a544dca08b5866525
Klaudia Beqo <37180828+KlaudiaBeqo@users.noreply.github.com>
2026-03-31 21:11:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-document.getElementById("donationForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("donationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    let plz = document.getElementById("plz").value;
+    let name = document.getElementById("name").value;
+    let item = document.getElementById("item").value;
 
-    if (!/^[0-9]{5}$/.test(plz)) {
-        document.getElementById("message").innerText = "Ungültige PLZ!";
-        return;
-    }
-
-    document.getElementById("message").innerText = "Spende erfolgreich registriert!";
+    document.getElementById("message").innerText =
+        "Danke " + name + "! Deine Spende (" + item + ") wurde registriert.";
 });
